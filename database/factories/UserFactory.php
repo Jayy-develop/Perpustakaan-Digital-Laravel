cce44ce658943812c60720388d1533dbb4538064
Jayy-develop <jayapenting92@gmail.com>
2026-04-30 13:43:42 +0700

    update user Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Types\This;
 
 /**
  * @extends Factory<User>
@@ -25,8 +26,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
